e9b8f4e228631e845b82db11088e3ade1a6a175b
Kai <k.davey4@icloud.com>
2025-11-08 14:56:29 -0800

    array proj 4 work

--- array.cpp
+++ array.cpp
@@ -14,6 +14,10 @@ int repeat(string a[], int n);
 int detectMatch(const string a[], int n, string target);
 int detectMax(const string a[], int n);
 int circleLeft(string a[], int n, int pos);
+int enumerateRuns(const string a[], int n);
+int flip(string a[], int n);
+int detectDifference(const string a1[], int n1, const string a2[], int n2);
+int subsequence(const string a1[], int n1, const string a2[], int n2);
 
 int main() {
 
@@ -25,7 +29,6 @@ int repeat(string a[], int n) {
     for (int i = 0; i < n; i++) {
         a[i] = a[i] + a[i];
     }
-
     return n;
 }
 
@@ -35,7 +38,6 @@ int detectMatch(const string a[], int n, string target) {
             return i;
         }
     }
-
     return -1;
 }
 
@@ -47,10 +49,70 @@ int detectMax(const string a[], int n) {
             max = i;
         }
     }
-
     return max;
 }
 
 int circleLeft(string a[], int n, int pos) {
+    if (pos > n - 1 || n < 0) return -1;
+
+    string temp = a[pos];
+
+    for (int i = pos; i < n - 1; i++) {
+        a[i] = a[i + 1];
+    }
+    a[n - 1] = temp;
+    return pos;
+}
+
+int enumerateRuns(const string a[], int n) {
+    if (n < 0) return -1;
+    if (n == 1) return 1;
+
+    int runs = 0;
+    string curr = a[0];
+    for (int i = 0; i < n; i++) {
+        if (a[i] != curr) {
+            runs++;
+        }
+        curr = a[i];
+    }
+    return runs;
+}
+
+int flip(string a[], int n) {
+    if (n < 0) return - 1;
+
+    string temp[n];
+    int i;
+    int j;
+
+    for (i = n - 1, j = 0; i >= 0; i--, j++) {
+        temp[j] = a[i];
+    }
+
+    for (i = 0; i < n; i++) {
+        a[i] = temp[i];
+    }
+    return n;
+}
+
+int detectDifference(const string a1[], int n1, const string a2[], int n2) {
+    if (n1 < 0 || n2 < 0) return -1;
+
+    int smallest = n1 < n2 ? n1 : n2;
+
+    int i = 0;
+    while (i < n1 && i < n2) {
+        if (a1[i] != a2[i]) {
+            return i;
+        }
+        i++;
+    }
+    return smallest;
+}
+
+int subsequence(const string a1[], int n1, const string a2[], int n2) {
+    if (n1 < 0 || n2 < 0) return -1;
+
 
 }
